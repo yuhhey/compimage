3255e6c56abe790db2509201949268ba20e4892b
grof <atiska1_fejlesztes@yahoo.com>
2013-09-13 15:53:43 +0200

    -g added. The workflow: - create symlinks - RAW->tif conversion (not with this script) - generate the composite image.

--- Python toolok/src/ImageSequence_cli.py
+++ Python toolok/src/ImageSequence_cli.py
@@ -7,12 +7,20 @@ from optparse import OptionParser
 def setup_config_parser():
     usage = "usage: %prog [options] raw_files_dir img_sequence_dir"
     config_parser = OptionParser(usage=usage)
-    config_parser.add_option('-t', '--sequence_type', dest='seq_type', default='HDR', help="Type of the sequences to identify. Valid values: HDR, TIME, PANO. (Default: %default)")
-    config_parser.add_option('-r', '--raw_ext', dest='raw_ext', default='.CR2', help="Extension of input files. (Default: %default)")
-    config_parser.add_option('-i', '--img_ext', dest='img_ext', default='.TIF', help="Extension of the image sequence output (Default: %default)")
-    config_parser.add_option('-p', '--prefix', dest='img_prefix', default='${dir}', help='Prefix of the img sequences identified. (Default: directory name of the input file')
-    config_parser.add_option('-R', '--recursive', action='store_true', dest='recursive', default=False, help='Traverses subdirs to find sequences. (Default: %default)')
-    config_parser.add_option('-m', '--maxdiff', type="int", dest='maxdiff', default=7, help="Maximum time difference of two images in a sequence. (Default: %default)")
+    config_parser.add_option('-g', '--generate', action=store_true, dest='generate', default=Fale,
+                             help="Type of the sequences to identify. Valid values: HDR, TIME, PANO. (Default: %default)")
+    config_parser.add_option('-t', '--sequence_type', dest='seq_type', default='HDR',
+                             help="Type of the sequences to identify. Valid values: HDR, TIME, PANO. (Default: %default)")
+    config_parser.add_option('-r', '--raw_ext', dest='raw_ext', default='.CR2',
+                             help="Extension of input files. (Default: %default)")
+    config_parser.add_option('-i', '--img_ext', dest='img_ext', default='.TIF',
+                             help="Extension of the image sequence output (Default: %default)")
+    config_parser.add_option('-p', '--prefix', dest='img_prefix', default='${dir}',
+                             help='Prefix of the img sequences identified. (Default: directory name of the input file')
+    config_parser.add_option('-R', '--recursive', action='store_true', dest='recursive', default=False,
+                             help='Traverses subdirs to find sequences. (Default: %default)')
+    config_parser.add_option('-m', '--maxdiff', type="int", dest='maxdiff', default=7,
+                             help="Maximum time difference of two images in a sequence. (Default: %default)")
     return config_parser
 
 
@@ -57,9 +65,11 @@ if config.recursive:
     # walk through
 else:
     img_seqs, single_imgs = collect_seq_strategy.parseDir(indir, seq_parser_config)
-    symlink_generator = CompositeImage.SymlinkGenerator()
+    if config.generate:
+        cmd = seq_generator
+    else:
+        cmd = CompositeImage.SymlinkGenerator()
     for cimg in img_seqs:
-        symlink_generator(cimg, seq_parser_config)
-        seq_generator(cimg, seq_parser_config)
+        cmd(cimg, seq_parser_config)
     
 
