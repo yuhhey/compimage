3a5a5387fe3fdf2a2b2ec4945ed673dfb644bd2c
grof <atiska1_fejlesztes@yahoo.com>
2013-09-20 15:39:16 +0200

    0.1 version.

---
+++ Python toolok/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='CompImg',
+      version='0.1dev',
+      packages=['src'],
+      license='Pisti',
+      long_description=open('README.md').read(),
+)
